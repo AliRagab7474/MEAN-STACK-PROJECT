92a1e0c23bfc01ce3283f2109bc7b66873d9dabe
Mohamed Tarek <mohamedtarekmostafa2005@gmail.com>
2026-08-23 03:58:51 +0300

    index - sended messages

--- backend/src/modules/message/message.controller.js
+++ backend/src/modules/message/message.controller.js
@@ -19,7 +19,40 @@ export const sendMessage = catchAsync(async(req,res,next)=>{
         return response.BadRequestException({message:"You Can Not Send Message(you are blocked)"})
     }
     
-    const message = await MessageModel.create({content,receiverId,senderId})
+    await MessageModel.create({
+        content,
+        receiverId,
+        senderId
+    });
 
-    return response.successesResponse({res,message:"Message Sent Successfully",data:message})
+    return response.successesResponse({res,message: "Message Sent Successfully",
+        data: {
+            receiver: `${receiver.FirstName} ${receiver.LastName}`,
+            content
+        }
+    });
+})
+
+//get user index
+
+export const getMessageReceived = catchAsync(async(req,res,next)=>{
+    const receiverId = req.user._id
+    const messages = await MessageModel.find({receiverId}).select("-senderId").sort({createdAt: -1})
+
+    if(messages.length === 0){
+        return response.NotFoundException({message:"The Index Is Empty"})
+    }
+    return response.successesResponse({res,message:"Your Received Messages Is ..",data:messages})
+})
+
+//get user sended messages
+
+export const getMessageSended = catchAsync(async(req,res,next)=>{
+    const senderId = req.user._id
+    const messages = await MessageModel.find({senderId}).populate("receiverId","FirstName LastName ").sort({createdAt: -1})
+
+    if(messages.length === 0){
+        return response.NotFoundException({message:"You Didn't Send Any Message"})
+    }
+    return response.successesResponse({res,message:"Your SendedMessages Is ..",data:messages})
 })
