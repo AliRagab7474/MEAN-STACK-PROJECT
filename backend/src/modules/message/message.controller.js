3ef8bd08d3d13ca2998add7fe38f5265f96f5986
Mohamed Tarek <mohamedtarekmostafa2005@gmail.com>
2026-09-16 18:30:46 +0300

    up

--- backend/src/modules/message/message.controller.js
+++ backend/src/modules/message/message.controller.js
@@ -63,25 +63,7 @@ export const getMessageReceived = catchAsync(async (req, res, next) => {
   });
 });
 
-//get user sended messages
 
-// export const getMessageSended = catchAsync(async (req, res, next) => {
-//   const senderId = req.user._id;
-//   const messages = await MessageModel.find({ senderId })
-//     .populate("receiverId", "FirstName LastName ")
-//     .sort({ createdAt: -1 });
-
-//   if (messages.length === 0) {
-//     return response.NotFoundException({
-//       message: "You Didn't Send Any Message",
-//     });
-//   }
-//   return response.successesResponse({
-//     res,
-//     message: "Your SendedMessages Is ..",
-//     data: messages,
-//   });
-// });
 
 
 
