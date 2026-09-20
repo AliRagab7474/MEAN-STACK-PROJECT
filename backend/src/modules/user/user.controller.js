88ef15563a878fad03601b3d3cd7477703db3ebf
Mohamed Tarek <mohamedtarekmostafa2005@gmail.com>
2026-09-20 14:32:35 +0300

    user fix

--- backend/src/modules/user/user.controller.js
+++ backend/src/modules/user/user.controller.js
@@ -76,7 +76,7 @@ export const shareProfile = catchAsync(async(req,res,next)=>{
         return response.ErrorResponse({message:"You Can't Share Your Profile"})
     }
     
-    const sharedLink =  `http://localhost:4200/u/${user._id}`
+    const sharedLink =  `http://sarhne-app.vercal.app/u/${user._id}`
     
     return response.successesResponse({res,message:"Link Profile Is Ready",data:sharedLink})
 })
